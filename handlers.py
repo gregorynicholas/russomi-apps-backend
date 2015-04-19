169a333f7f762fe66bc18417d571755a9b3a2cde
Michael Russo <russo.mail@gmail.com>
2015-04-18 22:58:12 -0400

    moved user_required decorator to handlers module

--- handlers.py
+++ handlers.py
@@ -1,4 +1,5 @@
 import logging
+
 import jinja2 as template
 
 import webapp2
@@ -8,6 +9,22 @@ from webapp2_extras.auth import InvalidAuthIdError
 from webapp2_extras.auth import InvalidPasswordError
 
 import os.path
+
+
+def user_required(handler):
+    """
+      Decorator that checks if there's a user associated with the current session.
+      Will also fail if there's no session present.
+    """
+
+    def check_login(self, *args, **kwargs):
+        auth = self.auth
+        if not auth.get_user_by_session():
+            self.redirect(self.uri_for('login'), abort=True)
+        else:
+            return handler(self, *args, **kwargs)
+
+    return check_login
 
 
 class BaseHandler(webapp2.RequestHandler):
